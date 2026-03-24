be56c779863679a1fa46e92d756a469ab5c9ae1d
Tuan Kieu <tuan.kieu.e@ctuav.vn>
2026-03-24 09:24:31 +0700

    add matmul using cublas and naive add_bias function

--- include/common.h
+++ include/common.h
@@ -4,3 +4,21 @@
 #include <cublasLt.h>
 #include <cudnn.h>
 
+cublasHandle_t cublas_handle;
+cublasLtHandle_t cublaslt_handle;
+
+#define CHECK_CUDA(call) { \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+        fprintf(stderr, "CUDA error in %s:%d: %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+        exit(EXIT_FAILURE); \
+    } \
+}
+
+#define CHECK_CUBLAS(call) { \
+    cublasStatus_t status = call; \
+    if (status != CUBLAS_STATUS_SUCCESS) { \
+        fprintf(stderr, "cuBLAS error in %s:%d: %d\n", __FILE__, __LINE__, status); \
+        exit(EXIT_FAILURE); \
+    } \
+}
--- include/matmul_forward.cu
+++ include/matmul_forward.cu
@@ -1,57 +1,96 @@
 #include "common.h"
-// 
+
+// SHAPE:
+// inp is (N,H,W), weight is (O,W), bias is (O)
+// out is (N,H,O)
+
 void matmul_cpu(float *out,
                 const float *inp, const float *weight, const float *bias,
-                int B, int T, int C, int OC){
-    
-    for (int b = 0 ; b < B; b++){
-        for (int t = 0 ; t < T; t++){
-            float *out_bt = out + b*T*OC + t*OC;
-            const float *inp_bt = inp + b*T*C + t*C;
-            for (int oc = 0 ; oc < OC; oc++) {
-                float val = (bias != NULL) ? bias[oc] : 0.0f;
-                const float *wrow = weight + oc*C;
-                for (int c ; c < C ; c++){
-                    val += wrow[c] * inp_bt[c];
+                int N, int H, int W, int O){
+    for (int n = 0; n < N; n++){
+        for (int h = 0; h < H; h++){
+            float *out_NH = out + n*H*O + h*O;
+            const float *inp_NH = inp + n*H*W + h*W;
+            for (int o; o < O; o++){
+                float val = (bias != nullptr) ? bias : 0.0f;
+                const float *wrow = weight + o*W;
+                for (int w = 0; w < W ; w++){
+                    val += inp_NH[w]*wrow[w];
                 }
-                out_bt[oc] = val;
-            }
+                out_NH[o] = val;
+            }  
         }
     }
 }
 
-// 
-__global__ void matmul_forward_kernel_naive(float *out,
+
+__global__ void matmul_naive_kernel_1(float *out,
                 const float *inp, const float *weight, const float *bias,
-                int BT, int C, int OC){
-    int bt = blockIdx.x * blockDim.x + threadIdx.x;
-    int oc = blockIdx.y * blockDim.y + threadIdx.y;
-    if (bt < BT && oc < OC) {
-        float val = (bias != NULL) ? bias[oc] : 0.0f;
-        const float* wrow = weight + oc * C;
-        const float* inp_bt = inp + bt * C;
-        for (int i = 0; i < C; i++) {
-            val += inp_bt[i] * wrow[i];
+                int N, int H, int W, int O){
+    int NH = blockIdx.x * blockDim.x + threadIdx.x;
+    int O = blockIdx.y * blockDim.y + threadIdx.y;
+    if (nh < NH && o < O){
+        float val = (bias != nullptr) ? bias : 0.0f;
+        const float *wrow = weight + o*W;
+        const float *inp_NH = inp + nh*W;
+        for (int w = 0; w < W; w++){
+            val += wrow[w]*inp_NH[w];
         }
-        out[bt * OC + oc] = val;
+        out[nh*O + o] = val;
     }
 }
 
-// 
-__global__ void matmul_forward_kernal_cublas(float *out,
-                const float *inp, const float *weight, const float *bias,
-                int B, int T, int C, int OC){
-
+__global__ void add_bias(float *out, float *bias, int N, int H, int O){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = idx; i < N * H * O; i+=stride){
+        int col = i % O;
+        out[i] += bias[col];
+    }
 }
 
-__global__ void add_bias(float *out, float *bias, int C, int T, int OC){
 
+void matmul_cublas(float *out,
+            const float *inp, const float *weight, const float *bias,
+                int N, int H, int W, int O, const sqrt_block_size){
+/*
+    cublasStatus_t cublasSgemm(cublasHandle_t handle,
+                            cublasOperation_t transa, cublasOperation_t transb,
+                            int m, int n, int k,
+                            const float           *alpha,
+                            const float           *A, int lda,
+                            const float           *B, int ldb,
+                            const float           *beta,
+                            float           *C, int ldc)
+
+    C = alpha*op(A) @ op(B) + beta*C;
+    inp is (N,H,W), weight is (O,W)
+    out is (N,H,O)
+    => out = inp @ weight.T
+    => out.T = weight @ inp.T 
+    but cublas use col-major => all tensor/matrix is transposed
+    => out.T = weight.T @ inp
+*/
+    const float alpha = 1.0f;
+    const float beta = 0.0f;
+    CHECK_CUBLAS(cublasSgemm(cublas_handle,
+                CUBLAS_OP_T, CUBLAS_OP_N,
+                O, N*H, W,
+                &alpha, 
+                weight, W,
+                inp, W,
+                &beta,
+                out, O));
+
+    if (bias != nullptr){
+        int block_size = sqrt_block_size * sqrt_block_size;
+        int grid_size = ceil_div(N*H*O, block_size);
+        add_bias<<<grid_size,block_size>>>(out, bias, N, H, O);
+        CHECK_CUDA(cudaGetLastError());
+    }
 }
 
-// 
-__global__ void matmul_forward_kernel_cublasLt(float *out,
-                const float *inp, const float *weight, const float *bias,
-                int B, int T, int C, int OC){
+void matmul_cublasLt(float *out, const float *inp, const float *weight, const float *bias,
+                    int N, int H, int W, int O){
 
 }
-
